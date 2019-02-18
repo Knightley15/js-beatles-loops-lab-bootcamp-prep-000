f3974e80ec6cda52cf7f9d516e25098464f491b1
Juliette Knightley <knightleyjb@gmail.com>
2019-02-18 01:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-function theBeatlesPlay(musicians, instruments){
-  var musicianString;
-  var musicianInstrument=[];
-  for(var i=0; i<musicians.length; i++){
-  musicianString= musicians[i] + ‘ plays ‘ + instruments[i];
-  musicianInstrument[i]=musicianString;
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return musicianInstrument;
+  return array;
 }
 
-const facts=[
-  “He was the last Beatle to learn to drive”,
-  “He was never a vegetarian”,
-  “He was a choir boy and boy scout”,
-  “He hated the sound of his own voice”
-  ];
-  
-function johnLennonFacts(factsArray){
-  var i=0;
-  var newArray=[];
-  while (factsArray.length > i){
-  newArray.push(`${factsArray[i]}` + “!!!”);
-  i++;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return newArray;
+  return results;
 }
 
-function iLoveTheBeatles(n){
-  var strings=[];
-  do{
-  n++;
-  strings.push(‘I love the Beatles!’)
-  }while (n < 15);
-  return strings;
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
 }
