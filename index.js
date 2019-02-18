c3ca2a5ab39d8c24ef022087de525ad735a7a2d4
Juliette Knightley <knightleyjb@gmail.com>
2019-02-18 01:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-function theBeatlesPlay(musicians,instruments) {
-  var x = []
-  for (var i = 0; i < musicians.length; i++) {
-    x.push(musicians[i] + ' plays ' + instruments[i])
-  }
-  return x;
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
 }
 
-function johnLennonFacts(array) {
-  var i = 0
-  while (i < array.length) {
-    array[i] += '!!!';
-    i++;
-  }
-  return array;
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
 
-function iLoveTheBeatles(number) {
-  var arr = [];
-  var i = 0;
-  do {
-    arr.push('I love the Beatles!');
-    i++;
-  } while (i <= number && number < 15);
-  return arr;// add solution here
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
